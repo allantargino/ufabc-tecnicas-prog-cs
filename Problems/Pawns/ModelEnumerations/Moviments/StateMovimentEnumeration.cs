d3dcd7c2505f9482932a66db074d691274575eed
Allan Targino <altargin@microsoft.com>
2018-03-08 19:46:15 -0300

    generating graph

---
+++ Problems/Pawns/ModelEnumerations/Moviments/StateMovimentEnumeration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pawns
+{
+    public class StateMovimentEnumeration
+    {
+        public IDictionary<State, IEnumerable<State>> ValidMovimentsPlayers { get; set; }
+    }
+}
--- Problems/Pawns/Program.cs
+++ Problems/Pawns/Program.cs
@@ -13,26 +13,24 @@ namespace Pawns
             // Player Configuration Enumeration
             var playerConfigurationGenerator = new PlayerConfigurationGenerator();
             var playerConfigurationEnumeration = playerConfigurationGenerator.Enumerate();
+            // Board Configuration Enumeration
+            var boardConfigurationGenerator = new BoardConfigurationGenerator(playerConfigurationEnumeration);
+            var BoardConfigurationEnumeration = boardConfigurationGenerator.Enumerate();
+            // State Configuration Enumeration
+            var stateConfigurationGenerator = new StateConfigurationGenerator(BoardConfigurationEnumeration);
+            var stateConfigurationEnumeration = stateConfigurationGenerator.Enumerate();
 
             // Player Moviment Enumeration
             var playerMovimentGenerator = new PlayerMovimentGenerator(playerConfigurationEnumeration);
             var playerMovimentEnumeration = playerMovimentGenerator.Enumerate();
-
-
-            // Board Configuration Enumeration
-            var boardColumnsGenerator = new BoardConfigurationGenerator(playerConfigurationEnumeration);
-            var BoardConfigurationEnumeration = boardColumnsGenerator.Enumerate();
-
             // Board Moviment Enumeration
             var boardMovimentGenerator = new BoardMovimentGenerator(playerMovimentEnumeration, BoardConfigurationEnumeration);
             var boardMovimentEnumeration = boardMovimentGenerator.Enumerate();
-
-
-            // State Configuration Enumeration
-
             // State Moviment Enumeration
-            //var graphGenerator = new GraphGenerator(graphValidMovimentsGenerator);
-            //graphGenerator.GenerateGraph();
+            var stateMovimentGenerator = new StateMovimentGenerator(stateConfigurationEnumeration, boardMovimentEnumeration);
+            var stateMovimentEnumeration = stateMovimentGenerator.Enumerate();
+
+            // Search Winning Strategy
 
             Console.WriteLine("Finished!");
             Console.ReadLine();
