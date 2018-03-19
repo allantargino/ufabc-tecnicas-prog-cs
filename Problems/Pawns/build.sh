5b5bec416bc3e12503742acd1a3d7466d83f5768
Allan Targino <altargin@microsoft.com>
2018-03-18 23:54:12 -0300

    script for building

---
+++ Problems/Pawns/build.sh
@@ -0,0 +1,3 @@
+dotnet restore /mnt/c/Repos/ufabc-tecnicas-prog-cs/Problems/Pawns
+dotnet publish /mnt/c/Repos/ufabc-tecnicas-prog-cs/Problems/Pawns -c Release -r ubuntu.16.04-x64 --self-contained
+export jogo=/mnt/c/Repos/ufabc-tecnicas-prog-cs/Problems/Pawns/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/Pawns
